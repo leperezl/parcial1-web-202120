cd68f223e61b97375939946b9902b968420c9bea
Luis Perez <le.perezl@uniandes.edu.co>
2021-10-01 14:31:10 -0500

    the axios

---
+++ src/axiostest.js
@@ -0,0 +1,8 @@
+const app = require('axios')
+async function getRequest(link) {
+    const response = await app.get(link)
+    return response.values;
+  };
+const pData = "https://gist.githubusercontent.com/jhonatan89/bc554ec3ded15407609714e8b7b8f1c8/raw/5ab1e3e5b45e99aabcbbdd30f55d2ae5eafb9cbe/nba-players";
+
+console.log(getRequest(pData))
--- src/controller.js
+++ src/controller.js
@@ -1,8 +1,19 @@
 const { response, request } = require('express');
+const axios = require('axios')
+
+
+const pData = "https://gist.githubusercontent.com/jhonatan89/bc554ec3ded15407609714e8b7b8f1c8/raw/5ab1e3e5b45e99aabcbbdd30f55d2ae5eafb9cbe/nba-players";
+
+async function getRequest(link) {
+  const response = await app.get(link)
+  return response.values;
+};
+
 
 const getPairsOfPlayers = async (req = request, resp = response, next) => {
   //Implementar logica aquí
-  return resp.json({ message: 'To be implemented' });
+  const data = await getRequest(pData)
+  return resp.json({ message: 'To be implemented ' + data});
 };
 
-module.exports = { getPairsOfPlayers };
+module.exports = { getPairsOfPlayers , getRequest};
