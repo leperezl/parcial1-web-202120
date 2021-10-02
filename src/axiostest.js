2f21af03cdfba19a9fdbdf9b94d6028cc608d90b
Luis Perez <le.perezl@uniandes.edu.co>
2021-10-01 22:56:19 -0500

    woah it was a dumb mistake

--- src/axiostest.js
+++ src/axiostest.js
@@ -1,8 +1,9 @@
 const app = require('axios')
 async function getRequest(link) {
     const response = await app.get(link)
-    return response.values;
+    console.log(response.data)
+    return response.data;
   };
 const pData = "https://gist.githubusercontent.com/jhonatan89/bc554ec3ded15407609714e8b7b8f1c8/raw/5ab1e3e5b45e99aabcbbdd30f55d2ae5eafb9cbe/nba-players";
 
-console.log(getRequest(pData))
+getRequest(pData)
--- src/controller.js
+++ src/controller.js
@@ -5,15 +5,22 @@ const axios = require('axios')
 const pData = "https://gist.githubusercontent.com/jhonatan89/bc554ec3ded15407609714e8b7b8f1c8/raw/5ab1e3e5b45e99aabcbbdd30f55d2ae5eafb9cbe/nba-players";
 
 async function getRequest(link) {
-  const response = await app.get(link)
-  return response.values;
+  const response = await axios.get(link)
+  //console.log(response.data)
+  return response.data;
 };
+
 
 
 const getPairsOfPlayers = async (req = request, resp = response, next) => {
   //Implementar logica aquí
-  const data = await getRequest(pData)
-  return resp.json({ message: 'To be implemented ' + data});
+  const r = await getRequest(pData)
+  //console.log("it seems to read")
+  let number=5
+  return resp.json({ message: r});
 };
-
+1
+const getPairs = (dat)=>{
+  return dat
+}
 module.exports = { getPairsOfPlayers , getRequest};
